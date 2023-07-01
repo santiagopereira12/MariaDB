4f043504ab9b3d6fff26d2587e3bad05dfb6726b
santiagopereira12 <spereira98@misena.edu.co>
2023-06-30 19:55:46 -0500

    STORED PROCEDURES

---
+++ Codigo/8-stored_procedures.sql
@@ -0,0 +1,17 @@
+
+DELIMITER //
+CREATE PROCEDURE calculate_distance_between_lines(
+    IN station_one POINT,
+    IN station_two POINT,
+    IN meters BOOLEAN
+)
+BEGIN
+    IF meters THEN
+        SELECT
+        ST_Distance_Sphere(station_one, station_two) AS Distances;
+    ELSE
+        SELECT
+        ST_Distance_Sphere(station_one, station_two) /1000 AS Distances;
+    END IF;
+END //
+DELIMITER ;
