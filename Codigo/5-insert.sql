68333401920ab20737c64d3eb4832310828f5143
santiagopereira12 <spereira98@misena.edu.co>
2023-06-30 19:50:35 -0500

    INSERT INTO

---
+++ Codigo/5-insert.sql
@@ -0,0 +1,17 @@
+-- Portales.
+
+INSERT INTO `portales` (name, color, referencia) VALUES ("Portal Norte", "Verde", "B");
+INSERT INTO `portales` (name, color, referencia) VALUES ("Portal Suba", "Amarillo", "C"),
+("Portal de la 80", "Morado", "D"), ("Portal Americas", "Rojo", "F"), ("Portal del Sur", "Azul", "G"), ("Portal Usme", "Naranja", "H"),
+("Portal Tunal", "Naranja", "H"), ("Portal Dorado", "Marron", "K"), ("Portal 20 de Julio", "Agua marina", "L");
+
+-- Rutas
+INSERT INTO `rutas` (type, year, id_portales) VALUES (7, 2023, 2);
+INSERT INTO `rutas` (type, year, id_portales) VALUES (17, 2023, 2),(19, 2023, 2),(25, 2023, 2),(30, 2023, 2),(50, 2023, 2),(73, 2023, 2);
+INSERT INTO `rutas` (type, year, id_portales) VALUES (10, 2023, 1),(11, 2023, 1),(12, 2023, 1),(13, 2023, 1),(16, 2023, 1),(18, 2023, 1),
+(23, 2023, 1),(27, 2023, 1),(26, 2023, 1),(28, 2023, 1),(46, 2023, 1),(50, 2023, 1),
+(55, 2023, 1),(71, 2023, 1),(72, 2023, 1),(74, 2023, 1),(75, 2023, 1);
+INSERT INTO `rutas` (type, year, id_portales) VALUES (6, 2023, 3),(10, 2023, 3),(20, 2023, 3),(21, 2023, 3),(22, 2023, 3),(24, 2023, 3),(81, 2023, 3);
+INSERT INTO `rutas` (type, year, id_portales) VALUES (19, 2023, 4),(23, 2023, 4),(26, 2023, 4),(28, 2023, 4),(32, 2023, 4),(51, 2023, 4),(60, 2023, 4);
+INSERT INTO `rutas` (type, year, id_portales) VALUES (11, 2023, 5),(12,2023,5),(22,2023,5),(30,2023,5),(43,2023,5),(45,2023,5),(46,2023,5),
+(47,2023,5),(52,2023,5);
